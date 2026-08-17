5dfb2575a410a55f175ea21ffe638c1cfde562ce
RAFAEL MACIEL MELO <rafael.melo31@senaisp.edu.br>
2026-08-17 10:12:58 -0300

    Initial commit

---
+++ src/main/java/com/example/cadastros/UsuarioController.java
@@ -0,0 +1,4 @@
+package com.example.cadastros;
+
+public class UsuarioController {
+}
---
+++ src/test/java/com/example/cadastros/CadastrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cadastros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
