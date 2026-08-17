098e074ae17cc07ac371c26a7b2206c82598658b
50927909898 <2022rmm@gmail.com>
2026-08-17 10:37:25 -0300

    primeiro commit

--- src/main/java/com/example/cadastros/Usuario.java
+++ src/main/java/com/example/cadastros/Usuario.java
@@ -1,4 +1,21 @@
 package com.example.cadastros;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Objects;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+
 public class Usuario {
+    private  String nome;
+    private  String email;
+    private  String cpf;
+
+
 }
--- src/main/java/com/example/cadastros/UsuarioController.java
+++ src/main/java/com/example/cadastros/UsuarioController.java
@@ -1,4 +1,26 @@
 package com.example.cadastros;
 
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/usuario")
+
+
+
+
 public class UsuarioController {
+    ArrayList <Usuario> usuarios = new ArrayList<>();
+
+ @GetMapping
+    public ArrayList<Usuario> listarTodosUsuarios(){
+    return usuarios;
+ }
+ @PostMapping
+    public Usuario cadastroUsuari(@RequestBody Usuario usuario){
+     usuarios.add(usuario);
+
+   return usuarios.getLast();
+ }
 }
