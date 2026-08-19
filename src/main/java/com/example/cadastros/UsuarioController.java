e3eb464c6c63c55e113a22866696e5d26c9874d1
50927909898 <2022rmm@gmail.com>
2026-08-19 11:54:12 -0300

    commit de atualização e excluir

--- src/main/java/com/example/cadastros/UsuarioController.java
+++ src/main/java/com/example/cadastros/UsuarioController.java
@@ -8,19 +8,38 @@ import java.util.ArrayList;
 @RequestMapping("/usuario")
 
 
-
-
 public class UsuarioController {
-    ArrayList <Usuario> usuarios = new ArrayList<>();
+    ArrayList<Usuario> usuarios = new ArrayList<>();
 
- @GetMapping
-    public ArrayList<Usuario> listarTodosUsuarios(){
-    return usuarios;
- }
- @PostMapping
-    public Usuario cadastroUsuari(@RequestBody Usuario usuario){
-     usuarios.add(usuario);
+    @GetMapping
+    public ArrayList<Usuario> listarTodosUsuarios() {
+        return usuarios;
 
-   return usuarios.getLast();
- }
+    }
+
+    @PostMapping
+    public Usuario cadastroUsuari(@RequestBody Usuario usuario) {
+        //RequestBody = faz a conversao
+        usuarios.add(usuario);
+
+        return usuarios.getLast();
+    }
+
+    @PutMapping("/{id}")
+    //PutMapping = é o caminho que ele vai puxar o id
+
+    public Usuario atualizarUsuario(@PathVariable int id, @RequestBody Usuario usuario) {
+        Usuario usuarioAtualizado = usuarios.get(id);
+        usuarioAtualizado.setNome(usuario.getNome());
+        usuarioAtualizado.setCpf(usuario.getCpf());
+        usuarioAtualizado.setEmail(usuario.getEmail());
+
+        return usuarioAtualizado;
+
+    }
+
+   @DeleteMapping("/{id}")
+    public void excluirUsuario( @PathVariable int id){
+        usuarios.remove(id);
+    }
 }
